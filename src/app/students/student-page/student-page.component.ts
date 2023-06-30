056b10187908b934d24dd33d9b4778421d976fd8
DeStRaaM <20destraam03@gmail.com>
2023-06-30 16:13:27 +0200

    Update student functionality added.

--- src/app/students/student-page/student-page.component.ts
+++ src/app/students/student-page/student-page.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
+import { Gender } from 'src/app/models/ui-models/gender.model';
 import { Students } from 'src/app/models/ui-models/student.model';
+import { GenderService } from 'src/app/services/gender.service';
 import { StudentService } from '../student.service';
 
 @Component({
@@ -22,22 +25,43 @@ export class StudentPageComponent {
         genderId: '',
         gender: { id: '', description: '' },
         address: { id: '', physicalAddress: '', postalAddress: '' },
-    }
+    };
+
+    genderList : Gender[] = []
   
     constructor(
       private readonly studentService: StudentService,
-      private readonly route: ActivatedRoute
+      private readonly route: ActivatedRoute,
+      private readonly genderService: GenderService,
+      private snackbar: MatSnackBar,
       ) { }
 
     ngOnInit(): void {
         this.route.paramMap.subscribe(params => {
             this.studentId = params.get('id');
-            if(this.studentId)
+            if(this.studentId){
                 this.studentService.getStudentById(this.studentId)
                 .subscribe((successResponse) => {
                     this.student = successResponse;
-                }); 
-            });
-    }
+                })
+                this.genderService.getGenders()
+                .subscribe((successResponse) => {
+                    this.genderList = successResponse;
+                })
+            };
+        }
+    )};
 
+    onUpdate(): void {
+        this.studentService.updateStudent(this.student.id, this.student)
+            .subscribe((successResponse) => {
+                this.snackbar.open("Student updated successfully", undefined, {
+                    duration: 2000,
+                });
+            },
+            (errorResponse) => {
+                console.log(errorResponse);
+            }
+        );
+    }
 }
--- src/app/students/student.service.ts
+++ src/app/students/student.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Students } from '../models/api-models/student.model';
+import { updateStudentRequest } from '../models/api-models/updateStudentRequest.models';
 
 @Injectable({
   providedIn: 'root'
@@ -18,5 +19,19 @@ export class StudentService {
 
   getStudentById(id: string): Observable<Students> {
     return this.httpClient.get<Students>(this.baseApiUrl + '/students/' + id);
+  }
+
+  updateStudent(studentId: string, studentRequest: Students): Observable<Students>{
+    const updateStudentRequest: updateStudentRequest = {
+      firstName: studentRequest.firstName,
+      lastName: studentRequest.lastName,
+      dateOfBirth: studentRequest.dateOfBirth,
+      email: studentRequest.email,
+      mobile: studentRequest.mobile,
+      genderId: studentRequest.genderId,
+      physicalAddress: studentRequest.address.physicalAddress,
+      postalAddress: studentRequest.address.postalAddress,
+    }
+    return this.httpClient.put<Students>(this.baseApiUrl + '/students/' + studentId, updateStudentRequest);
   }
 }
